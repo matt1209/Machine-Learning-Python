d0962663d59f093c5d1a6177e29cda28773a6577
ZIHAO.ZHAO <ZIHAO.ZHAO@delta.corp>
2017-05-09 17:25:20 +0800

    best fit slope

--- linear regression.py
+++ linear regression.py
@@ -7,10 +7,17 @@ Created on Mon May  8 13:34:15 2017
 
 import pandas as pd
 import quandl
-import math
+import math, datetime
 import numpy as np
 from sklearn import preprocessing, cross_validation, svm
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+import pickle
+
+
+style.use('ggplot')
+
 
 df = quandl.get("WIKI/GOOGL")
 
@@ -28,21 +35,54 @@ forecast_out = int(math.ceil(0.01*len(df)))
 
 df['label'] = df[forecast_col].shift(-forecast_out)
 
-df.dropna(inplace=True)
+
 
 X = np.array(df.drop('label',1))
-y = np.array(df['label'])
-
 X = preprocessing.scale(X)
+X_lately = X[-forecast_out:]
+X = X[:-forecast_out]
 
+
+df.dropna(inplace=True)
 y = np.array(df['label'])
+y = np.array(df['label'])
+
 
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X,y,test_size=0.2)
 
 # clf = LinearRegression()
 # clf = svm.SVR()
-clf = svm.SVR(kernel='poly')
+# clf = svm.SVR(kernel='poly')
+clf = LinearRegression(n_jobs=-1)
 clf.fit(X_train,y_train)
+
+# save the model and avoid re-train the model
+with open('linearregression.pickle','wb') as f:
+    pickle.dump(clf,f)
+    
+pickle_in = open('linearregression.pickle','rb')
+clf = pickle.load(pickle_in)
+
 accuracy = clf.score(X_test,y_test)
 
-print(accuracy)
+forecast_set = clf.predict(X_lately)
+
+# print(forecast_set, accuracy, forecast_out)
+
+# change forecast_set to nan except Forecast colum
+df['Forecast'] = np.nan
+last_date = df.iloc[-1].name 
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
+    
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
